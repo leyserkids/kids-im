ca6c2f8e74673d4198d87fe5c18f6d1872dd22e1
alvin huang <huanguolin@outlook.com>
2026-01-17 18:57:07 +0800

    fix eslint issue

--- sdk-js-wasm/.eslintrc.js
+++ sdk-js-wasm/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
+  ignorePatterns: ['*.js', 'scripts/**', 'assets/**'],
   extends: [
     'eslint:recommended',
     'plugin:node/recommended',
